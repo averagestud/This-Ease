34db49698041c00ba14cdf964b16d4fa411dd057
Deepu <deepudpu04@gmail.com>
2024-02-05 20:49:21 +0530

    Adding the files to the repository
    This is the first commit to the repository. The files are added to the repository

---
+++ 1_🗿_main.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
+import streamlit as st
+import joblib as jb 
+import matplotlib.pyplot as plt
+os.chdir('C:/Users/Deepu/AI and Python/This-Ease')
+
