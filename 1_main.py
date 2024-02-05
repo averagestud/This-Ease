12bc53ff74214e49b08c40008bb5024d0a41e625
Deepu <deepudpu04@gmail.com>
2024-02-05 22:39:43 +0530

    Hearts completed

--- 1_main.py
+++ 1_main.py
@@ -6,4 +6,6 @@ import joblib as jb
 import matplotlib.pyplot as plt
 os.chdir('C:/Users/Deepu/AI and Python/This-Ease')
 
+st.title("This-Ease: An app to predict any disease using Machine Learning")
 
+st.write("Kindly navigate to the 'Hearts' page to predict the presence of heart disease")
